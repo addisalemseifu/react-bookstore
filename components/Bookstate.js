2a630b22930c347d3cd1cc70e85c086924858875
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-05-05 17:27:24 +0300

    Setup bookstore.

---
+++ components/Bookstate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Bookstate() {
+  return (
+    <div>Bookstate</div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" />
+      <Route path="/categories" />
+    </Routes>
+
+  );
+}
+
+export default App;
